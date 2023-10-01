e766b16221b7e2d0e2b5c3f527e04a82d4408e4d
ariceliom <contato@ariceliomoura.com>
2023-10-01 16:38:27 -0300

    upload do projeto

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+
+  <meta name="HandheldFriendly" content="true">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+
+  <!-- Título do site -->
+  <title>Aricelio Moura | Software Engineer</title>
+  <meta property="og:title" content="Nome do Site">
+
+  <!-- Descrição do site -->
+  <meta name="description" content="Página de apresentação para biolink do meu portfólio de desenvolvimento web.">
+  <meta itemprop="description" content="Página de apresentação para biolink do meu portfólio de desenvolvimento web.">
+  <meta property="og:description" content="Página de apresentação para biolink do meu portfólio de desenvolvimento web.">
+
+  <!-- Cor tema do site -->
+  <meta name="theme-color" content="#FF6600">
+  <meta name="msapplication-TileColor" content="#FF6600">
+  <meta name="msapplication-navbutton-color" content="#FF6600">
+  <meta name="apple-mobile-web-app-status-bar-style" content="#FF6600">
+
+  <!-- Favicons do site -->
+  <link rel="icon" type="image/png" sizes="16x16" href="./files/favicons/favicon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./files/favicons/favicon.png">
+  <link rel="icon" type="image/png" sizes="194x194" href="./files/favicons/favicon.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="./files/favicons/favicon.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="./files/favicons/favicon.png">
+  <link rel="mask-icon" href="./files/favicons/safari-pinned-tab.svg" color="#FF6600">
+  <link rel="shortcut icon" href="./files/favicons/favicon.png">
+  <meta name="msapplication-TileImage" content="./files/favicons/favicon.png">
+
+  <!-- Tamanho imagem compartilhamento -->
+  <meta property="og:locale" content="pt_BR">
+  <meta property="og:image:type" content="image/png">
+  <meta property="og:image:width" content="1920">
+  <meta property="og:image:height" content="1080">
+  <meta property="og:type" content="website">
+  <meta property="og:card" content="summary_large_image">
+
+  <!-- Imagem de compartilhamento -->
+  <meta name="image" content="./files/favicons/favicon.png">
+  <meta itemprop="image" content="./files/favicons/favicon.png">
+  <meta property="og:image" content="./files/favicons/favicon.png">
+
+  <!-- Nome do Site -->
+  <meta name="author" content="Aricelio Moura | Software Engineer">
+  <meta itemprop="name" content="Aricelio Moura | Software Engineer">
+  <meta name="article:author" content="Aricelio Moura | Software Engineer">
+  <meta name="application-name" content="Aricelio Moura | Software Engineer">
+  <meta property="og:site_name" content="Aricelio Moura | Software Engineer">
+  <meta name="apple-mobile-web-app-title" content="Aricelio Moura | Software Engineer">
+
+  <!-- Importação do CSS -->
+  <link href="./assets/css/pages/index/index.css" rel="stylesheet">
+</head>
+<body>
+
+  <!-- Seção -->
+  <section class="apresentacao">
+    <!-- Efeito de Faixa -->
+    <div class="faixa"></div>
+    <!-- Conteúdo -->
+    <div class="conteudo">
+      <!-- Efeito de Blush -->
+      <div class="blush"></div>
+      <!-- Foto -->
+      <div class="foto">
+        <div class="linha"></div>
+        <img src="./files/images/perfil.jpeg">
+      </div>
+      <!-- Informação -->
+      <div class="informacao">
+        <h2>Aricelio Moura</h2>
+        <p>Software Engineer | Web developer</p>
+      </div>
+      <!-- Links -->
+      <div class="links">
+        <!-- Link -->
+        <a href="https://www.instagram.com/ariceliomoura/" target="_blank">
+          <div class="icone">
+            <img src="./files/images/icone-instagram.png">
+          </div>
+          <div class="texto">
+            <h3>@ariceliomoura</h3>
+            <p>Siga meu perfil</p>
+          </div>
+        </a>
+        <!-- Link -->
+        <a href="https://www.ariceliomoura.com/" target="_blank">
+          <div class="icone">
+            <img src="./files/images/icone-site.png">
+          </div>
+          <div class="texto">
+            <h3>ariceliomoura.com</h3>
+            <p>Acesse meu portfólio</p>
+          </div>
+        </a>
+        <!-- Link -->
+        <a href="https://wa.me/5585982170079?text=Olá,%20venho%20do%20BioLink%20Instagram." target="_blank">
+          <div class="icone">
+            <img src="./files/images/icone-whatsapp.png">
+          </div>
+          <div class="texto">
+            <h3>Chame no Whatsapp</h3>
+            <p>Para contato</p>
+          </div>
+        </a>
+        <!-- Link -->
+        <a href="https://cursodev.ariceliomoura.com/" target="_blank">
+          <div class="icone">          
+            <img src="./files/images/curso.png">
+          </div>
+          <div class="texto">
+            <h3>Curso Dev</h3>
+            <p>Quer criar seu próprio site?</p>
+          </div>
+        </a>
+        <!-- Link -->
+        <a href="https://cursodev.ariceliomoura.com/" target="_blank" class="destaque">
+          <h4>EXCLUSIVO</h4>
+          <div class="texto">
+            <h3>Promoção exclusiva</h3>
+            <p>Para você que vem do meu site tem desconto especial nos cursos de tecnologias!</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <!-- Importação do Javascript -->
+  <script src="./assets/js/global/global.js"></script>
+
+</body>
+</html>
